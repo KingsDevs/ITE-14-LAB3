cc39cbb380f81adb71cc26911160c00a5e44297b
KingsDevs <sunnyckair@gmail.com>
2022-04-20 17:12:20 +0800

    hapit na jud

--- bst.c
+++ bst.c
@@ -205,10 +205,17 @@ bool delete_node(node * n, int data)
             node * ps = get_predecessor(inorder_seq, n);
             if(ps == NULL)
                 ps = get_successor(inorder_seq, n);
-
+            
+            int temp_data = n->data;
             n->data = ps->data;
 
+            if(temp_data > n->data)
+                delete_node(n->left, n->data);
+            else
+                delete_node(n->right, n->data);
 
+            free(inorder_seq->list);
+            free(inorder_seq);
         }
     }
     else if(n->data < data)
@@ -242,59 +249,59 @@ int main(int argc, char const *argv[])
     // }
     
 
-    // while (1)
-    // {
-    //     printf("1. Display Tree\n");
-    //     printf("2. Insert Node\n");
-    //     printf("3. Delete Node\n");
-    //     printf("4. Exit\n");
-    //     printf(">> ");
+    while (1)
+    {
+        printf("1. Display Tree\n");
+        printf("2. Insert Node\n");
+        printf("3. Delete Node\n");
+        printf("4. Exit\n");
+        printf(">> ");
 
-    //     int choice;
-    //     scanf(" %d", &choice);
+        int choice;
+        scanf(" %d", &choice);
 
-    //     if(choice == 1)
-    //     {
-    //         if(root == NULL || root->empty)
-    //             printf("The tree is empty!\n");
-    //         else
-    //             display_tree(root);
-    //     }
-    //     else if(choice == 2)
-    //     {
-    //         printf("Enter a number to insert\n>> ");
-    //         int num;
-    //         scanf(" %d", &num);
+        if(choice == 1)
+        {
+            if(root == NULL || root->empty)
+                printf("The tree is empty!\n");
+            else
+                display_tree(root);
+        }
+        else if(choice == 2)
+        {
+            printf("Enter a number to insert\n>> ");
+            int num;
+            scanf(" %d", &num);
 
-    //         if(root == NULL)
-    //             root = create_node(num);
-    //         else
-    //             insert_node(root, num);
-    //     }
-    //     else if(choice == 3)
-    //     {
-    //         if(root == NULL)
-    //         {
-    //             printf("The tree is empty!\n");
-    //             continue;
-    //         }
+            if(root == NULL)
+                root = create_node(num);
+            else
+                insert_node(root, num);
+        }
+        else if(choice == 3)
+        {
+            if(root == NULL)
+            {
+                printf("The tree is empty!\n");
+                continue;
+            }
 
-    //         printf("Enter a number to delete\n>> ");
-    //         int num;
-    //         scanf(" %d", &num);
+            printf("Enter a number to delete\n>> ");
+            int num;
+            scanf(" %d", &num);
 
-    //         bool isfound = delete_node(root, num);
+            bool isfound = delete_node(root, num);
 
-    //         if(isfound)
-    //             printf("The node successfully deleted!\n");
-    //         else
-    //             printf("Node not found!\n");
+            if(isfound)
+                printf("The node successfully deleted!\n");
+            else
+                printf("Node not found!\n");
 
-    //     }
-    //     else
-    //         break;
+        }
+        else
+            break;
     
-    // }
+    }
     
 
     return 0;
